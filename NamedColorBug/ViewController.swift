8732ebfc136bdf5c0022ee508ef795ced3bf75d6
omochimetaru <omochi.metaru@gmail.com>
2019-02-08 17:44:32 +0900

    init

---
+++ NamedColorBug/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var innocentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        innocentView.backgroundColor = UIColor.blue
+    }
+
+}
+
